1278cb6d4fedbd41a876c6c56c42a1a6f7f5e949
RodrigoSHL <shl.catalan@gmail.com>
2022-09-13 17:53:08 -0300

    feat: v.1 redis implementation
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { PokeapiModule } from './pokeapi/pokeapi.module';
 
 @Module({
-  imports: [PokeapiModule],
+  imports: [PokeapiModule, CacheModule.register({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
--- src/pokeapi/pokeapi.controller.ts
+++ src/pokeapi/pokeapi.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+  Controller,
+  Get,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
 import { PokeapiService } from './pokeapi.service';
 
 @Controller('pokeapi')
 export class PokeapiController {
   constructor(private readonly pokeapiService: PokeapiService) {}
 
+  @UseInterceptors(CacheInterceptor) // Automatically cache the response for this endpoint
+  @CacheTTL(30) // override TTL to 30 seconds
+  @CacheKey('custom-key')
   @Get(':id')
   getNameByPokeId(@Param('id') id: string) {
     return this.pokeapiService.getNameByPokeId(id);
