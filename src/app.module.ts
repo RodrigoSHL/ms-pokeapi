e620b35f0b0c5fc8a87e768341ee17da0c464037
RodrigoSHL <shl.catalan@gmail.com>
2022-09-13 20:56:28 -0300

    implement redis in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { PokeapiModule } from './pokeapi/pokeapi.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [PokeapiModule, CacheModule.register({ isGlobal: true })],
+  imports: [
+    PokeapiModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
