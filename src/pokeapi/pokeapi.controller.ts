c3c8a98bd3a62e7f10778ff9f47c76ddcc27112a
RodrigoSHL <shl.catalan@gmail.com>
2022-09-13 20:57:48 -0300

    fix: add cache key and ttl

--- src/pokeapi/pokeapi.controller.ts
+++ src/pokeapi/pokeapi.controller.ts
@@ -1,21 +1,13 @@
-import {
-  CacheInterceptor,
-  CacheKey,
-  CacheTTL,
-  Controller,
-  Get,
-  Param,
-  UseInterceptors,
-} from '@nestjs/common';
+import { CacheKey, CacheTTL, Controller, Get, Param } from '@nestjs/common';
 import { PokeapiService } from './pokeapi.service';
 
 @Controller('pokeapi')
 export class PokeapiController {
   constructor(private readonly pokeapiService: PokeapiService) {}
 
-  @UseInterceptors(CacheInterceptor) // Automatically cache the response for this endpoint
+  // @UseInterceptors(CacheInterceptor) // Automatically cache the response for this endpoint // use this decorator when you want a caching controller or specific method
   @CacheTTL(30) // override TTL to 30 seconds
-  @CacheKey('custom-key')
+  @CacheKey('poke-key')
   @Get(':id')
   getNameByPokeId(@Param('id') id: string) {
     return this.pokeapiService.getNameByPokeId(id);
