86e803a57e415e5113401e7cf3a2ece1c058fcf2
RodrigoSHL <shl.catalan@gmail.com>
2022-09-14 10:10:47 -0300

    feat: add key<T> in get cache manager

--- src/pokeapi/pokeapi.service.ts
+++ src/pokeapi/pokeapi.service.ts
@@ -1,9 +1,6 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { IPokeapi } from './interface/pokeapi.interface';
-import { IPokemon } from './interface/pokemon.interface';
 import { Cache } from 'cache-manager';
-import { HttpModule } from '@nestjs/axios';
 
 @Injectable()
 export class PokeapiService {
@@ -13,10 +10,12 @@ export class PokeapiService {
   ) {}
   async getNameByPokeId(id: string): Promise<any> {
     try {
-      const cachedData = await this.cacheManager.get(id.toString());
+      const cachedData = await this.cacheManager.get<{ name: string }>(
+        id.toString(),
+      );
       if (cachedData) {
         console.log(`Getting data from cache!`);
-        return `${cachedData}`;
+        return `${cachedData.name}`;
       }
       // if not, call API and set the cache:
       const { data } = await this.httpService.axiosRef.get(
